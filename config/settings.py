5b923c4cb5e45824227069a2f539219b28d73693
Evelyn Cardoso <vyamaral@gmail.com>
2026-06-11 17:19:32 -0300

    debug ativado para correção do erro

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv(
     "django-insecure-rpgmanager-dev-2026"
 )
 
-DEBUG = os.getenv("DEBUG", "False") == "False"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "webapp-aula13-hrdzddd2ezbxa9bv.eastus-01.azurewebsites.net",
