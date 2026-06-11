c912de5111b7b7db0c91b8a90670f53fb2368fa5
Evelyn Cardoso <vyamaral@gmail.com>
2026-06-11 12:05:16 -0300

    teste logs

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
         "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", "5432"),
+        print("DB_HOST =", os.getenv("DB_HOST"))
+        print("DB_NAME =", os.getenv("DB_NAME"))
+        print("DB_USER =", os.getenv("DB_USER"))
+        print("TESTE =", os.getenv("TESTE"))
     }
 }
 
