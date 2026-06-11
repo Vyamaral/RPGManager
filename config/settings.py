1259c99671fc26cbc990e0842e8f80e4dca35922
Evelyn Cardoso <vyamaral@gmail.com>
2026-06-11 12:06:19 -0300

    teste logs

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+raise Exception("SETTINGS CARREGADO")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
